1246287c6055cb42b988750da7225aeffce2b59c
유채원 <ohwow5@sookmyung.ac.kr>
2021-08-01 17:53:45 +0900

    Update email login func(#1)

--- app/src/main/java/com/example/moming/LoginActivity.kt
+++ app/src/main/java/com/example/moming/LoginActivity.kt
@@ -1,12 +1,113 @@
 package com.example.moming
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.facebook.CallbackManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import javax.security.auth.callback.Callback
 
 class LoginActivity: AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+        callbackManager = CallbackManager.Factory.create()
+
+        initLoginButton()
+        initSignUpButton()
+        initEmailAndPasswordEditText()
+    }
+
+    private fun initLoginButton(){
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){ task ->
+                    if (task.isSuccessful){
+                        finish()
+                    }else{
+                        Toast.makeText(this, "로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    //TODO: 회원 가입 처리 다른 창에서 진행
+    private fun initSignUpButton(){
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        signUpButton.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+            Log.d("id / password", email + "/" + password)
+
+            if(password.length < 6 ){
+                Toast.makeText(this, "비밀번호는 6자 이상으로 설정해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){task ->
+                    Log.d(">>>>>>>", task.result.toString())
+
+                    if (task.isSuccessful){
+                        Toast.makeText(this, "회원가입을 성공했습니다. 로그인 버튼을 눌러주세요.", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+        }
+    }
+
+    private fun initEmailAndPasswordEditText(){
+        val emailEditText = findViewById<EditText>(R.id.emailEditTxt)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditTxt)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+
+        emailEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signUpButton.isEnabled = enable
+        }
+
+        passwordEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signUpButton.isEnabled = enable
+        }
+    }
+
+
+    private fun getInputEmail(): String{
+        return findViewById<EditText>(R.id.emailEditTxt).text.toString()
+    }
+
+    private fun getInputPassword(): String{
+        return findViewById<EditText>(R.id.passwordEditTxt).text.toString()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        return super.onTouchEvent(event)
     }
 }
